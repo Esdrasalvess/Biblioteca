36269adadcc5bd3d3b632293d09a1f5a017892f5
Esdrás <esdrasalvesdossantos2002@gmail.com>
2024-11-13 20:13:40 -0300

    teste

--- Biblioteca_V2/src/main/java/com/meiobyte/biblioteca_v2/WebConfig.java
+++ Biblioteca_V2/src/main/java/com/meiobyte/biblioteca_v2/WebConfig.java
@@ -1,6 +1,5 @@
 package com.meiobyte.biblioteca_v2;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
