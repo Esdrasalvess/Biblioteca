b4f30a0d4e45cbc10a0d63f51e87668c4a762ee6
Maria Letícia A. Gonçalves <105677223+mllmale@users.noreply.github.com>
2024-11-13 20:27:16 -0300

    Update EmprestimoRepository.java

--- Biblioteca-main/Biblioteca_V2/src/main/java/com/meiobyte/biblioteca_v2/repository/EmprestimoRepository.java
+++ Biblioteca-main/Biblioteca_V2/src/main/java/com/meiobyte/biblioteca_v2/repository/EmprestimoRepository.java
@@ -2,14 +2,22 @@ package com.meiobyte.biblioteca_v2.repository;
 
 import com.meiobyte.biblioteca_v2.model.Emprestimo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
 
-    List<Emprestimo> findByLeitorId(Integer leitorId);
-    
-    List<Emprestimo> findByFuncionarioId(Integer funcionarioId);
-    
-    List<Emprestimo> findByDataBetween(String dataInicial, String dataFinal);
-    
-    List<Emprestimo> findByStatus(String status);
+    @Query("SELECT * FROM Emprestimo WHERE id_leitor = :leitorId;")
+    List<Emprestimo> buscarPorLeitor(@Param("leitorId") Integer leitorId);
+
+    @Query("SELECT * FROM Emprestimo WHERE id_funcionario = :funcionarioId;")
+    List<Emprestimo> buscarPorFuncionario(@Param("funcionarioId") Integer funcionarioId);
+
+    @Query("SELECT * FROM Emprestimo WHERE data_emprestimo BETWEEN :dataInicial AND :dataFinal;")
+    List<Emprestimo> buscarPorData(@Param("dataInicial") String dataInicial, @Param("dataFinal") String dataFinal);
+
+    @Query("SELECT * FROM Emprestimo WHERE status = :status;")
+    List<Emprestimo> buscarNaoEmprestado(@Param("status") String status);
 }
