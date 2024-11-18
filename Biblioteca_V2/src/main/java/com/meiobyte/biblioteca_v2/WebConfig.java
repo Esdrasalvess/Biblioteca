429c9bebe1de371551c20b72187a35948c1f35b1
El0wki <92699365+El0wki@users.noreply.github.com>
2024-11-18 20:50:11 -0300

    Revert "Merge branch 'main' of https://github.com/Esdrasalvess/Biblioteca"
    This reverts commit db25a1490a99dccd2cd635407f8ef9ef02f4d7a3, reversing
    changes made to ec6cce76e482bf1df2dfd6089f136bddf8e3b4a2.

--- Biblioteca_V2/src/main/java/com/meiobyte/biblioteca_v2/WebConfig.java
+++ Biblioteca_V2/src/main/java/com/meiobyte/biblioteca_v2/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8088") // METE a URL do front aqui
+                .allowedOrigins("http://localhost:8081") // METE a URL do front aqui
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
